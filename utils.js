c19eec09e0588770771c3275e42f71449174d5c8
TyroneYvesChen <243307272@qq.com>
2018-09-23 20:40:42 +0800

    'utils.js修改'

--- utils.js
+++ utils.js
@@ -45,8 +45,47 @@ function getDocumentPort() {
     }
 }
 
+// 判断是否是dom对象
+function isDomElement(obj) {
+    return !!(obj && (obj.nodeType == 1 || obj.nodeType == 9));
+}
+
+// 节流
+function throttle(func, wait) {
+    var timeout, context, args, result;
+    var previous = 0;
+
+    var later = function () {
+        previous = +new Date();
+        timeout = null;
+        func.apply(context, args)
+    };
+
+    var throttled = function () {
+        var now = +new Date();
+        //下次触发 func 剩余的时间
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        // 如果没有剩余的时间了或者你改了系统时间
+        if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            func.apply(context, args);
+        } else if (!timeout) {
+            timeout = setTimeout(later, remaining);
+        }
+    };
+    return throttled;
+}
+
 module.exports = {
     getElementTop,
     getViewPort,
-    getDocumentPort
+    getDocumentPort,
+    isDomElement,
+    throttle
 }
