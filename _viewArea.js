613f1edc94aa915e71a7da627d8fd9f6ab6bd2c3
TyroneYvesChen <243307272@qq.com>
2018-09-24 15:35:27 +0800

    '删除不需要的utils代码'

--- _viewArea.js
+++ _viewArea.js
@@ -115,40 +115,6 @@
         return elemTop
     }
 
-    /*视口的大小，部分移动设备浏览器对innerWidth的兼容性不好，需要
-    *document.documentElement.clientWidth或者document.body.clientWidth
-    *来兼容（混杂模式下对document.documentElement.clientWidth不支持）。
-    *使用方法 ： getViewPort().width
-    */
-    function getViewPort() {
-        if (document.compatMode == "BackCompat") {   //浏览器嗅探，混杂模式
-            return {
-                width: document.body.clientWidth,
-                height: document.body.clientHeight
-            }
-        } else {
-            return {
-                width: document.documentElement.clientWidth,
-                height: document.documentElement.clientHeight
-            }
-        }
-    }
-
-    //获得文档的大小（区别与视口）, 用法和上面相同
-    function getDocumentPort() {
-        if (document.compatMode == "BackCompat") {
-            return {
-                width: document.body.scrollWidth,
-                height: document.body.scrollHeight
-            }
-        } else {
-            return {
-                width: Math.max(document.documentElement.scrollWidth, document.documentElement.clientWidth),
-                height: Math.max(document.documentElement.scrollHeight, document.documentElement.clientHeight)
-            }
-        }
-    }
-
     // 判断是否是dom对象
     function isDomElement(obj) {
         return !!(obj && (obj.nodeType == 1 || obj.nodeType == 9));
@@ -187,8 +153,6 @@
     }
 
     var utils = {
-        getViewPort: getViewPort,
-        getDocumentPort: getDocumentPort,
         getElementTop: getElementTop,
         isDomElement: isDomElement,
         throttle: throttle
